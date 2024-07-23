2094a8287f1d1b6214d27065849c96dcfc42119f
arbaz <arbaz@gmail.com>
2024-07-23 14:40:44 +0530

    updated tf file

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,4 @@
+/*
 output "cluster_name" {
   description = "Amazon Web Service EKS Cluster Name"
   value       = module.eks.cluster_name
@@ -18,3 +19,5 @@ output "cluster_security_group_id" {
   description = "Security group ID for the Amazon Web Service EKS Cluster "
   value       = module.eks.cluster_security_group_id
 }
+
+*/
